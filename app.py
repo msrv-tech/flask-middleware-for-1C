a6ac55e2599767bab2b0fa68ce2cc1d9b5c43824
PCHOME\Александр <mav84@bk.ru>
2025-03-18 18:26:57 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, session
 from config import Config
 from extensions import db, migrate
-from models import User, Product  # Импорт моделей после инициализации
+from api.products import products_bp
+from api.cart import cart_bp
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Инициализация расширений
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Импорт API маршрутов
-    from api.products import products_bp
-    app.register_blueprint(products_bp)
+    # Регистрация API
+    app.register_blueprint(products_bp, url_prefix='/api')
+    app.register_blueprint(cart_bp, url_prefix='/api')
 
-    from api.cart import cart_bp
-    app.register_blueprint(cart_bp)
+    # Веб-маршруты
+    @app.route('/')
+    def index():
+        from models import Product
+        products = Product.query.all()
+        return render_template('index.html', products=products)
+
+    @app.route('/cart')
+    def cart():
+        cart_items = session.get('cart', {})
+        return render_template('cart.html', cart=cart_items)
 
     return app
 
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000, debug=True)
